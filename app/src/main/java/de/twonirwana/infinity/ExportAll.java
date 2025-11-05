d018ad9f39b7e976c436a5d1f01f3c3313d74c42
Murray Carr <onekilo@gmail.com>
2025-11-05 22:38:06 +0000

    Force a refresh to get all files.

--- app/src/main/java/de/twonirwana/infinity/ExportAll.java
+++ app/src/main/java/de/twonirwana/infinity/ExportAll.java
@@ -3,10 +3,17 @@ package de.twonirwana.infinity;
 
 import java.io.IOException;
 
+/**
+ * Exports all data in different formats
+ * @author twonirwana
+ * Export Downloads files: multiple CSV types for sectorials and factions.
+ * The CSVs are then parsed to download unit logos, unit image assets.
+ * Then the static html pages can be generated.
+ */
 public class ExportAll {
 
     public static void main(String[] args) throws IOException {
-        Database db = new DatabaseImp();
+        Database db = new DatabaseImp(true);
 
         CsvPrinter.printAll(db);
 
