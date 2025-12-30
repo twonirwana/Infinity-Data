df340e7d2d88997bb2b2b0a07784fee3e5defea1
twonirwana <jvs@mailbox.org>
2025-12-30 14:18:55 +0100

    playwright test

--- web/src/test/java/de/twonirwana/infinity/PlaywrightScreenshotTest.java
+++ web/src/test/java/de/twonirwana/infinity/PlaywrightScreenshotTest.java
@@ -10,9 +10,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -20,13 +20,17 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class PlaywrightScreenshotTest {
 
     static Playwright playwright;
-    static Browser browser;
+    static Browser chromium;
+    static Browser firefox;
+    static Browser webkit;
     static String baseUrl;
     BrowserContext context;
     Page page;
@@ -43,7 +47,9 @@ public class PlaywrightScreenshotTest {
         System.out.println("Test server running at: " + baseUrl);
 
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        chromium = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        firefox = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        webkit = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @AfterAll
@@ -51,20 +57,34 @@ public class PlaywrightScreenshotTest {
         playwright.close();
     }
 
-    @BeforeEach
-    void createContext() {
-        context = browser.newContext(new Browser.NewContextOptions()
-                .setViewportSize(1920, 1080));
-        page = context.newPage();
+    private static Stream<Arguments> generateTestData() {
+        return Arrays.stream(BrowserTyp.values())
+                .flatMap(b -> Arrays.stream(HtmlPrinter.Template.values())
+                        .map(t -> Arguments.of(b, t)));
+    }
+
+    private Browser fromType(BrowserTyp browserTyp) {
+        if (browserTyp == BrowserTyp.FIREFOX) {
+            return firefox;
+        } else if (browserTyp == BrowserTyp.CHROMIUM) {
+            return chromium;
+        } else if (browserTyp == BrowserTyp.WEBKIT) {
+            return webkit;
+        }
+        throw new IllegalArgumentException("Unknown browser type: " + browserTyp);
     }
 
     @ParameterizedTest
-    @CsvSource({"a4_image", "c6onA4_image", "a4_overview", "a7_image", "letter_image", "card_bw"})
-    void testCssRefactoringMatchesBaseline(String style) throws IOException {
+    @MethodSource("generateTestData")
+    void testCssRefactoringMatchesBaseline(BrowserTyp browserType, HtmlPrinter.Template template) throws IOException {
+        Browser browser = fromType(browserType);
+        context = browser.newContext(new Browser.NewContextOptions()
+                .setViewportSize(1920, 1080));
+        page = context.newPage();
         page.navigate(baseUrl + "/");
         page.waitForLoadState();
         assertThat(page.locator("body")).isVisible();
-        page.getByLabel("Select Card Style:").selectOption(style);
+        page.getByLabel("Select Card Style:").selectOption(template.name());
         page.getByLabel("Your army code:").fill("hE4Mc2hpbmRlbmJ1dGFpASCBkAIBAQAKAISIAQEAAIcaAQIAAIU1AQQAAIdSAQEAAIcZAQQAAICeAQEAAIcdAQUAAICnAQMAAIcbAQMAAIccAQMAAgEABwCHHwECAACG%2FAEBAACGIQEFAACAoQEBAACA4AGC5QAAgKIBAgAAhyMBAgA%3D");
 
 
@@ -76,18 +96,23 @@ public class PlaywrightScreenshotTest {
         byte[] actualImageBytes = newPage.screenshot(new Page.ScreenshotOptions()
                 .setFullPage(true));
 
-
+        String fileName = browser.browserType().name() + "_" + template.name();
+        File expectedFile = new File("playwright/expected/" + fileName + "_expected.png");
         BufferedImage actual = ImageIO.read(new ByteArrayInputStream(actualImageBytes));
-        BufferedImage expected = ImageIO.read(new File("playwright/expected/" + style + "_expected.png"));
+        if (!expectedFile.exists()) {
+            ImageIO.write(actual, "png", new File("playwright/" + fileName + "_expected.png"));
+            Assertions.fail();
+        }
 
+        BufferedImage expected = ImageIO.read(expectedFile);
         ImageComparisonResult result = new ImageComparison(expected, actual)
                 .setPixelToleranceLevel(0.1)
                 .compareImages();
 
 
         if (result.getImageComparisonState() != ImageComparisonState.MATCH) {
-            ImageIO.write(result.getResult(), "png", new File("playwright/" + style + "_diff_" + System.currentTimeMillis() + ".png"));
-            ImageIO.write(actual, "png", new File("playwright/" + style + "_actual_" + System.currentTimeMillis() + ".png"));
+            ImageIO.write(result.getResult(), "png", new File("playwright/" + fileName + "_diff_" + System.currentTimeMillis() + ".png"));
+            ImageIO.write(actual, "png", new File("playwright/" + fileName + "_actual_" + System.currentTimeMillis() + ".png"));
         }
 
         Assertions.assertThat(result.getImageComparisonState()).isEqualTo(ImageComparisonState.MATCH);
@@ -96,5 +121,11 @@ public class PlaywrightScreenshotTest {
     @AfterEach
     void closeContext() {
         context.close();
+    }
+
+    private static enum BrowserTyp {
+        CHROMIUM,
+        FIREFOX,
+        WEBKIT
     }
 }
